b0461f2001cfee38950e83923cee8a5190ea2770
Dominik <dominik-blut@web.de>
2021-10-15 19:22:47 +0200

    Fix for new TTS version

--- server.py
+++ server.py
@@ -31,7 +31,7 @@ def create_synthesizer(model_args):
 	if os.path.isfile(vocoder_config_file):
 		model_args['vocoder_config'] = vocoder_config_file
 
-	synthesizer = Synthesizer(model_args['tts_checkpoint'], model_args['tts_config'], model_args['vocoder_checkpoint'], model_args['vocoder_config'])
+	synthesizer = Synthesizer(model_args['tts_checkpoint'], model_args['tts_config'], None, model_args['vocoder_checkpoint'], model_args['vocoder_config'])
 	return synthesizer
 
 path = "./.models.json"
@@ -73,28 +73,19 @@ def tts():
 	file_path = "{}/files/{}".format(os.getcwd(), text_hash)
 	files_dir = "{}/files".format(os.getcwd())
 
-	if(os.path.isdir(files_dir) == False):
-		os.makedirs(files_dir)
-
-	if(os.path.isfile(file_path)):
-		file = open(file_path, "rb")
-		out = io.BytesIO(file.read())
-		file.close()
-		out.seek(0)
+	if(request.args.get('language')):
+		language = request.args.get('language')
 	else:
-		if(request.args.get('language')):
-			language = request.args.get('language')
-		else:
-			language = "german"
+		language = "german"
 
-		synthesizer = globals()[language]
-		wavs = synthesizer.tts(text)
-		out = io.BytesIO()
-		synthesizer.save_wav(wavs, out)
+	synthesizer = globals()[language]
+	wavs = synthesizer.tts(text)
+	out = io.BytesIO()
+	synthesizer.save_wav(wavs, out)
 
-		file = open(file_path, "wb")
-		file.write(out.getbuffer())
-		file.close()
+	file = open(file_path, "wb")
+	file.write(out.getbuffer())
+	file.close()
 
 	audio_hash = hashlib.sha256(repr(out).encode('utf-8')).hexdigest()
 
